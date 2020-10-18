36ba09755de44f70482a8b3b23d5eaf66a60c0b4
Adrian Leal <adrianlealalonse@gmail.com>
2020-10-17 22:06:08 -0500

    Created templates and basic styling

--- club_site/urls.py
+++ club_site/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('/projects', views.projects, name='projects'),
+    path('/join', views.join, name='join'),
+    path('/contact', views.contact, name='contact'),
 ]
--- club_site/views.py
+++ club_site/views.py
@@ -4,3 +4,14 @@ def home(request):
     context = {}
     return render(request, 'club_site/home.html', context)
 
+def projects(request):
+    context = {}
+    return render(request, 'club_site/projects.html', context)
+
+def join(request):
+    context = {}
+    return render(request, 'club_site/join.html', context)
+
+def contact(request):
+    context = {}
+    return render(request, 'club_site/contact.html', context)
